3491928fb003fbaa22e5c165ad0f1b3229d09f55
Andrey <kotlyar.andrey03@mail.ru>
2024-07-20 18:00:29 +0300

    Вёрстка блока features

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 const menu = document.querySelector('#menu'),
-      navbar = document.querySelector('.navbar');
+      navbar = document.querySelector('.navbar'),
+      close = document.querySelectorAll('.close');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+});
+
+close.forEach(e => {
+    e.addEventListener('click', () => {
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('active');
+    });
 });
